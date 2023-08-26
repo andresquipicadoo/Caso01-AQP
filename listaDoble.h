a809a8af254bc3113d83b17fa43f51d6f807f258
andresquipicadoo <aquirosp592@gmail.com>
2023-08-25 19:05:50 -0600

    Preliminar1-AndresQuirosPicado

---
+++ listaDoble.h
@@ -0,0 +1,73 @@
+
+
+#include <iostream>
+using namespace std;
+
+class nodoLista{
+    public:
+
+
+};
+typedef nodoLista* pnode;
+
+
+class Lista_Doble{
+  public:
+     Lista_Doble() {first=NULL;}
+     ~Lista_Doble();
+
+
+    void insertar(int fecha,string titular,string cuerpo , string url){
+        //Este metodo se encarga de insertar una nueva noticia en la lista ya sea en el incio final o en
+        // cualquier otra posicion.
+    }
+
+    int  largoLista(){
+        //Este metodo se encarga de recorrer la lista  desde el primer elemento de la lista hasta
+        // el ultimo elemento realizandolo  a traves de los nodos.
+
+    }
+    void buscar(string palabra){
+
+        // Este metodo se encarga de buscar un elemento(Titular)  determinado de la lista que se
+        //   hasta encontrar ese elemento.
+
+    }
+    void buscarFecha(int fecha){
+       // Este metodo se encarga de buscar la fecha de una noticia  determinada de la lista que se
+      // va recorriendo  hasta encontrar esa fecha . Este metodo va ser importante para el metodo buscarNoticia() de noticias.h.
+      
+
+
+    }
+    void buscarPosicion(int dato){
+        //Este metodo se encarga de buscar  en que posicion de la lista se encuentra
+        //la  noticia .
+    }
+    void mostrar (){
+
+        //Este metodo se encarga  de mostrar todas las noticias  de la lista .
+    }
+    void mostrarPosicion(int posicion){
+        //Este metodo se encarga de mostrar en que posicion de la lista se encuentra la notica .
+    }
+    void borrarElemento(string palabra){
+
+        // Este metodo se encarga de borrar una noticia de la lista lo cual
+        // lo realiza recorriendo la lista hasta encontrar el elemento a buscar para finalmente borrarlo.
+    }
+    bool listaVacia(){
+
+        // Este metodo se encarga de validar si la lista se encuentra vacia o no.
+    }
+  private:
+    pnode first;
+
+
+
+
+
+
+
+};
+
